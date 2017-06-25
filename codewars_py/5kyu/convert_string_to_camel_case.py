ce46c37ff3a3c3602b561af6cc931abbb3a03f56
Navjot <nsingh@localhost.localdomain>
2017-06-25 10:40:49 -0400

    problem complete

--- codewars_py/5kyu/convert_string_to_camel_case.py
+++ codewars_py/5kyu/convert_string_to_camel_case.py
@@ -6,5 +6,7 @@ def to_camel_case(text):
     split_str = re.sub(r"-|_", " ", text).split()
     return split_str[0] + "".join([char[0].upper() + char[1:] for char in split_str[1:]])
 
-print(to_camel_case(''))
-print to_camel_case("the_stealth_warrior")
+
+# Best
+def to_camel_case(s):
+    return s[0] + s.title().translate(None, "-_")[1:] if s else s
