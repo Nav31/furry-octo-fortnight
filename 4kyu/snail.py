1b3bce9bf92819a37f5f6b0fb43a2c5c5ee06ba1
NSingh <nsingh3104@gmail.com>
2017-10-04 19:25:44 -0400

    complete

--- 4kyu/snail.py
+++ 4kyu/snail.py
@@ -4,33 +4,22 @@
 
 def snail(an_array):
     ret_list = []
-    # iterate over the top list
 
     def sub_snail(a_array):
         if len(a_array) == 0 or a_array == [[]]:
             return ret_list
-
         if len(a_array) == 1:
             ret_list.append(a_array[0][0])
             return ret_list
-
         for char in a_array[0]:
             ret_list.append(char)
         a_array.pop(0)
-
-        # iterate over the right most column
         for sub_arr in a_array:
             ret_list.append(sub_arr.pop())
-
-        # iterate over the bottom array of-course must reverse or...
         for i in range(len(a_array[-1])-1, -1, -1):
             ret_list.append(a_array[-1][i])
         a_array.pop()
-
-        # iterate from the remaining
         for sub_arr in reversed(a_array):
             ret_list.append(sub_arr.pop(0))
-
         return sub_snail(a_array)
-
     return sub_snail(an_array)
